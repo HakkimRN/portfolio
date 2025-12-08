b3c8cac8ddef85ef82f42d41a74311f410a5699a
HakkimRN <hakkim.r.n@gmail.com>
2025-12-08 15:08:32 +0530

    More responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
-
   return (
     <nav className="nav">
-      <h1>H</h1>
-      <Link className="button" to="/" draggable="false">About</Link>
-      <Link className="button" to="/projects" draggable="false">Projects</Link>
-      <Link className="button" to="/resume" draggable="false">Resume</Link>
+      <NavLink className="button" to="/" draggable="false">
+        About
+      </NavLink>
+      <NavLink className="button" to="/projects" draggable="false">
+        Projects
+      </NavLink>
+      <NavLink className="button" to="/resume" draggable="false">
+        Resume
+      </NavLink>
     </nav>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,13 +9,13 @@ const Sidebar = () => {
     <>
       <nav>
         <div className="sidebar">
-          <Link to="https://github.com/HakkimRN">
+          <Link  to="https://github.com/HakkimRN" data-label="Github">
             <img className="logo" src={GITHUB_URL} />
           </Link>
-          <Link to="https://www.linkedin.com/in/hakkim-rustom-naina-516051256">
+          <Link to="https://www.linkedin.com/in/hakkim-rustom-naina-516051256" data-label="LinkedIn">
             <img className="logo" src={LINKEDIN_URL} />
           </Link>
-          <Link to="">
+          <Link to="" data-label="Fiverr">
             <img className="logo" src={FIVERR_URL} />
           </Link>
         </div>
