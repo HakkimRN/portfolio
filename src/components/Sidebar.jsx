a742f08b49b460aaa423f5b9c0ebef1aad9347bc
HakkimRN <hakkim.r.n@gmail.com>
2026-06-03 16:50:27 +0530

    renamed asset name

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import GITHUB_URL from "../assets/github-mark-white.png";
-import LINKEDIN_URL from "../assets/inBug-White.png";
+import LINKEDIN_URL from "../assets/linkedin-white.png";
 
 const Sidebar = () => {
   return (
@@ -12,7 +12,7 @@ const Sidebar = () => {
             <img
               className="logo"
               src={GITHUB_URL}
-              aria-label="Github Profile"
+              alt="Github Profile"
             />
           </Link>
           <Link
@@ -22,7 +22,7 @@ const Sidebar = () => {
             <img
               className="logo"
               src={LINKEDIN_URL}
-              aria-label="Linkedin Profile"
+              alt="Linkedin Profile"
             />
           </Link>
         </div>
